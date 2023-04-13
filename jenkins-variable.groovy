2787a4df7702eb265d0cc076eaa7efdf9f68dc94
Amit9158113578 <mandhareamit37@gmail.com>
2023-04-13 09:20:12 +0000

    changes in variable file

--- jenkins-variable.groovy
+++ jenkins-variable.groovy
@@ -1,13 +1,13 @@
 def jenkinsVariables = [
-  AWS_DEFAULT_REGION: "your_aws_region",
-  AWS_ACCESS_KEY_ID: "your_aws_access_key_id",
-  AWS_SECRET_ACCESS_KEY: "your_aws_secret_access_key",
-  DOCKER_REGISTRY: "your_docker_registry",
-  IMAGE_NAME: "your_image_name",
-  ECS_CLUSTER: "your_ecs_cluster_name",
-  ECS_SERVICE: "your_ecs_service_name",
-  CODECOMMIT_REPO: "your_codecommit_repo_name",
-  CODECOMMIT_BRANCH: "your_codecommit_branch_name"
+  AWS_DEFAULT_REGION: "ap-southeast-1",
+  AWS_ACCESS_KEY_ID: "AKIA3SA4HLZ3A73VBH2D",
+  AWS_SECRET_ACCESS_KEY: "QLdsRvglJ916O535iNaPPioA7G93h/+q9nQvlC8K",
+  DOCKER_REGISTRY: "my-image-repo",
+  IMAGE_NAME: "myimage",
+  ECS_CLUSTER: "MY-ECS-cluster",
+  ECS_SERVICE: "my-ecs-service",
+  CODECOMMIT_REPO: "Hello-world",
+  CODECOMMIT_BRANCH: "master"
 ]
 
 return jenkinsVariables
